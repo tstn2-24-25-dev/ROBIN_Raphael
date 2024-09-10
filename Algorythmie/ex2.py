7bf18706f86e9c8d9d76e48d1276c9cc0c6fec10
Raphael Robin <raphael.robin31000@gmail.com>
2024-09-10 10:03:48 +0200

    First Commit, ex1 Done

---
+++ Algorythmie/ex2.py
@@ -0,0 +1,11 @@
+def main():
+    return
+
+def readFile(path): 
+    with open(path, ) as file:
+        firstLine = file.readline()
+        content = file.readlines()
+    return (firstLine,content)
+
+if __name__ =="__main__":
+    main()
