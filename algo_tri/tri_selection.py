4b09c3c8a73f116fa4fc772c62d809a60858c867
Raphael Robin <raphael.robin31000@gmail.com>
2024-09-24 10:25:09 +0200

    tri selection done

--- algo_tri/tri_selection.py
+++ algo_tri/tri_selection.py
@@ -1,5 +1,30 @@
 def main():
-    pass
+    l = [5,4,1,6,3,2]
+    print(f"unsorted: {l}")
+    sortedL = tri_selection(l)
+    print(f"sorted:   {sortedL}")
+
+# def selectSort(l):
+#     if len(l)>1:
+#         for minIndex in range(0,len(l)-1):
+#             for i in range(minIndex+1,len(l)):
+#                 if l[i]<l[minIndex]:
+#                     l[i],l[minIndex] = l[minIndex],l[i]
+#     return l
+
+
+def indiceMin(tab, j):
+    indice_Min = j
+    for indice in range (j+1,len(tab)):
+        if tab[indice]<tab[indice_Min]:
+            indice_Min = indice
+    return indice_Min
+
+def tri_selection(tab):
+    for indice in range (len(tab)):
+        indice_Min = indiceMin(tab,indice)
+        tab[indice], tab[indice_Min] = tab[indice_Min], tab[indice]
+    return tab
 
 if __name__=="__main__":
     main()
